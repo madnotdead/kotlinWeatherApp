6bf1e10294baf52151152c0bc2b8a3f4d0f55f51
madnotdead <leandromaguna@gmail.com>
2017-07-19 00:39:35 -0300

    Added Detail, new Extensions, RequestDayForecastCommand.

---
+++ app/src/main/java/com/example/leandrom/myapplication/domain/commands/RequestDayForecastCommand.kt
@@ -0,0 +1,7 @@
+package com.example.leandrom.myapplication.domain.commands
+
+/**
+ * Created by madnotdead on 7/17/17.
+ */
+class RequestDayForecastCommand {
+}
---
+++ app/src/main/java/com/example/leandrom/myapplication/extensions/ContextExtension.kt
@@ -0,0 +1,5 @@
+package com.example.leandrom.myapplication.extensions
+
+/**
+ * Created by madnotdead on 7/18/17.
+ */
